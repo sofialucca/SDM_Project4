9808d66d2c2f5bf407ba481232c16b7d2ad4826e
sofialucca <lsofy99@gmail.com>
2022-11-06 09:26:03 -0600

    working

--- app/src/main/java/edu/cs478/sofialucca/project4/MainActivity.java
+++ app/src/main/java/edu/cs478/sofialucca/project4/MainActivity.java
@@ -23,14 +23,101 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<TextView> textViewArray;
     static final int PLAYER_MOVE_1 = 1;
     static final int PLAYER_MOVE_2 = 2;
+    static final int FIRST_MOVE = 0;
     private ThreadP1 t1;
     private ThreadP2 t2;
     private TextView textWinner;
+    private final Handler uiHandler1 = new Handler(Looper.getMainLooper()) {
+        public void handleMessage(Message msg) {
+            int what = msg.what ;
+            boolean winner = false;
+            int count = 0;
+            Log.i("UI", "Move received "+ msg.arg1);
+            switch (what) {
+                case PLAYER_MOVE_1:
+
+                    positions.put(msg.arg1,1);
+                    textViewArray.get(msg.arg1).setBackgroundResource(R.drawable.player_1);
+                    for(int i = 0; i<9;i++) {
+
+                        if (positions.get(i) != 0) {
+                            count++;
+                            if (((i == 0 || i % 3 == 0) && positions.get(i) == 1 && positions.get(i + 1) == 1 && positions.get(i + 2) == 1) || ((i <= 2) && positions.get(i) == 1 && positions.get(i + 3) == 1 && positions.get(i + 6) == 1)) {
+                                //Toast.makeText(this,"Player "+ player +" is the winner",Toast.LENGTH_LONG).show();
+                                winner = true;
+                                break;
+                            }
+                        }
+                    }
+                    if(winner){
+                        textWinner.setText("GAME OVER: PLAYER 1 WINS");
+                        textWinner.setTextColor(getResources().getColor(R.color.purple_700));
+                    }else if(count == positions.size()) {
+                        textWinner.setText("GAME OVER: TIE");
+                        textWinner.setTextColor(getResources().getColor(R.color.black));
+                    }else{
+                        Message newMsg;
+                        newMsg = t2.myHandler2.obtainMessage(PLAYER_MOVE_1);
+                        newMsg.arg1 = msg.arg1;
+                        newMsg.sendToTarget();
+
+                    }
+
+
+                    break;
+
+            }
+
+        }
+    };
+    private final Handler uiHandler2= new Handler(Looper.getMainLooper()) {
+        public void handleMessage(Message msg) {
+            int what = msg.what ;
+            boolean winner = false;
+            int count = 0;
+
+            switch (what) {
+                case PLAYER_MOVE_2:
+                    positions.put(msg.arg1,2);
+                    //TODO change interface
+                    textViewArray.get(msg.arg1).setBackgroundResource(R.drawable.player_2);
+
+                    for(int i = 0; i<9;i++){
+
+                        if(positions.get(i) != 0){
+                            count++;
+                            if(((i == 0 || i%3 == 0) && positions.get(i+1) == 2 && positions.get(i+2) == 2 && positions.get(i) == 2) || ((i <=2 ) && positions.get(i) == 2 && positions.get(i+3) == 2 && positions.get(i+6) == 2)){
+                                //Toast.makeText(this,"Player "+ player +" is the winner",Toast.LENGTH_LONG).show();
+                                winner = true;
+                                break;
+                            }
+                        }
+
+                    }
+                    if(winner){
+                        textWinner.setText("GAME OVER: PLAYER 2 WINS");
+                        textWinner.setTextColor(getResources().getColor(R.color.red));
+                    }else if(count == positions.size()) {
+                        textWinner.setText("GAME OVER: TIE");
+                        textWinner.setTextColor(getResources().getColor(R.color.black));
+                    }else{
+                        Message newMsg;
+                        newMsg = t1.myHandler1.obtainMessage(PLAYER_MOVE_2);
+                        newMsg.arg1 = msg.arg1;
+                        newMsg.sendToTarget();
+
+                    }
+                    break;
+            }
+
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button bttnNewGame = findViewById(R.id.btnNewGame);
+        positions = new HashMap<Integer,Integer>();
         for(int i =0; i< 9;i++){
             positions.put(i,0);
 
@@ -47,87 +134,36 @@ public class MainActivity extends AppCompatActivity {
         textViewArray.add((TextView) findViewById(R.id.text_8));
         textWinner = findViewById(R.id.textWinner);
         t1 = new ThreadP1();
-        t1.start();
         t2 = new ThreadP2();
-        t2.start();
-    }
-    private final Handler mHandler = new Handler(Looper.getMainLooper()) {
-        public void handleMessage(Message msg) {
-            int what = msg.what ;
-            boolean winner = false;
-            int count = 0;
+        bttnNewGame.setOnClickListener(v -> {
 
-            switch (what) {
-                case PLAYER_MOVE_1:
-                    positions.put(msg.arg1,1);
-                    //TODO change interface
-                    textViewArray.get(msg.arg1).setBackgroundResource(R.drawable.player_1);
-                    for(int i = 0; i<9;i++) {
-
-                        if (positions.get(i) != 0) {
-                            count++;
-                            if (((i == 0 || i % 3 == 0) && positions.get(i + 1) == 1 && positions.get(i + 2) == 1) || ((i <= 2) && positions.get(i + 3) == 1 && positions.get(i + 6) == 1)) {
-                                //Toast.makeText(this,"Player "+ player +" is the winner",Toast.LENGTH_LONG).show();
-                                winner = true;
-                                break;
-                            }
-                        }
-                    }
-                        if(winner){
-                            textWinner.setText("GAME OVER: PLAYER 1 WINNER");
-                        }else if(count == positions.size()) {
-                            textWinner.setText("GAME OVER: TIE");
-                        }else{
-                            Message newMsg;
-                            newMsg = t2.myHandler2.obtainMessage(PLAYER_MOVE_1);
-                            newMsg.arg1 = msg.arg1;
-                            newMsg.sendToTarget();
-
-                        }
-
-
-                    break;
-                case PLAYER_MOVE_2:
-                    positions.put(msg.arg1,2);
-                    //TODO change interface
-                    for(int i = 0; i<9;i++){
-                        textViewArray.get(msg.arg1).setBackgroundResource(R.drawable.player_2);
-
-                        if(positions.get(i) != 0){
-                            count++;
-                            if(((i == 0 || i%3 == 0) && positions.get(i+1) == 2 && positions.get(i+2) == 2) || ((i <=2 ) && positions.get(i+3) == 2 && positions.get(i+6) == 2)){
-                                //Toast.makeText(this,"Player "+ player +" is the winner",Toast.LENGTH_LONG).show();
-                                winner = true;
-                                break;
-                            }
-                        }
-
-                    }
-                    if(winner){
-                        textWinner.setText("GAME OVER: PLAYER 1 WINNER");
-                    }else if(count == positions.size()) {
-
-                        textWinner.setText("GAME OVER: TIE");
-                    }else{
-                        Message newMsg;
-                        newMsg = t1.myHandler1.obtainMessage(PLAYER_MOVE_1);
-                        newMsg.arg1 = msg.arg1;
-                        newMsg.sendToTarget();
-
-                    }
-                    break;
+            if(t1.isAlive() || t2.isAlive()){
+                Looper.getMainLooper().quit();
             }
+            for(int i = 0; i<9; i++){
+                textViewArray.get(i).setBackgroundResource(R.drawable.border);
+                positions.put(i,0);
+            }
+            Looper.getMainLooper().loop();
+            t1.start();
+            t2.start();
+        });
 
-        }
-    };
+    }
+
     public class ThreadP1 extends Thread {
 
         public Handler myHandler1;
-        private HashMap<Integer,Integer> positions;
+        //private HashMap<Integer,Integer> positions;
+        private ArrayList<Integer> positions;
+        Random random;
         public void run(){
+            Log.i("THREAD1", "STARTED");
             Looper.prepare();
+            positions = new ArrayList<>(9);
+            random = new Random();
             for(int i =0; i< 9;i++){
-                positions.put(i,0);
+                positions.add(i);
             }
 
             myHandler1 = new Handler(){
@@ -139,36 +175,38 @@ public class MainActivity extends AppCompatActivity {
                             } catch (InterruptedException e) {
                                 Log.i("THREAD1", "Woken up");
                             }
-                            positions.put(msg.arg1,2);
-                            int i;
-                            for(i =0; i< 9;i++){
-                                if(positions.get(i) == 0){
-                                    break;
-                                }
-                            }
 
-                            positions.put(i,1);
-                            Message msgMove = Message.obtain();
-                            Message newMsg = mHandler.obtainMessage(PLAYER_MOVE_1);
-                            newMsg.arg1 = i;
-                            newMsg.arg2 = 1;
+                            positions.remove(positions.indexOf(msg.arg1));
+                            int i;
+
+                            //i = (int)Math.random()*positions.size();
+                            i = random.nextInt(positions.size());
+
+                            Message newMsg = uiHandler1.obtainMessage(PLAYER_MOVE_1);
+                            newMsg.arg1 = positions.get(i);
+                            Log.i("THREAD 1","Move " +newMsg.arg1);
+                            positions.remove(i);
+                            newMsg.sendToTarget();
                             break;
                     }
 
                 }
             };
 
-
+            //int i =(int) Math.random()*9;
+            int i = random.nextInt(9);
+            positions.remove(i);
+            Message newMsg = uiHandler1.obtainMessage(PLAYER_MOVE_1);
+            newMsg.arg1 = i;
+            newMsg.sendToTarget();
+            Log.i("THREAD 1","Move " +i);
             Looper.loop();
-
+/*
             try {
                 Thread.sleep(3000);
             } catch (InterruptedException e) {
                 Log.i("THREAD1", "Woken up");
-            }
-
-
-
+            }*/
         }
     }
     public class ThreadP2 extends Thread {
@@ -177,6 +215,7 @@ public class MainActivity extends AppCompatActivity {
         HashMap<Integer,Integer> positions;
         public void run(){
             Looper.prepare();
+            positions = new HashMap<Integer,Integer>();
             for(int i =0; i< 9;i++){
                 positions.put(i,0);
             }
@@ -200,8 +239,11 @@ public class MainActivity extends AppCompatActivity {
                                     empty = false;
                                 }
                                 if(positions.get(i) == 0){
-                                    firstFree = i;
-                                    if((i-1>0 && positions.get(i-1) == 2 )|| (i+1 <9 && positions.get(i+1) == 2) || (i+3<9 && positions.get(i+3)== 2)||(i-3>0 && positions.get(i-3) == 2)){
+                                    if(firstFree == -1){
+                                        firstFree = i;
+                                    }
+
+                                    if((i-1>0 && i%3 != 0 && positions.get(i-1) == 2 )|| (i+1 <9 && i%2!=0 && positions.get(i+1) == 2) || (i+3<9 && positions.get(i+3)== 2)||(i-3>0 && positions.get(i-3) == 2)){
                                         newPos = i;
                                         break;
                                     }
@@ -218,7 +260,7 @@ public class MainActivity extends AppCompatActivity {
                                 newPos = firstFree;
                             }
                             positions.put(newPos,2);
-                            Message newMsg = mHandler.obtainMessage(PLAYER_MOVE_2);
+                            Message newMsg = uiHandler2.obtainMessage(PLAYER_MOVE_2);
                             newMsg.arg1 = newPos;
                             newMsg.sendToTarget();
                             break;
@@ -226,16 +268,13 @@ public class MainActivity extends AppCompatActivity {
 
                 }
             };
-
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                Log.i("THREAD1", "Woken up");
-            }
-
             Looper.loop();
 
-
+    /*        try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                Log.i("THREAD2", "Woken up");
+            }*/
 
 
 
